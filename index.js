5ea9cdda024118e00ca3aff94908abe1859c97f5
Tess Huang <tesshuangxj@yahoo.com>
2021-01-11 20:20:24 -0800

    Add pdf endpoint and pdf generator function

--- index.js
+++ index.js
@@ -2,6 +2,45 @@ const express = require("express");
 const puppeteer = require("puppeteer");
 const app = express();
 
+// Task: 1. launch a brower 2. goto the url 3. generate a pdf 4. close browser
+
+app.get("/pdf", async (req, res) => {
+
+  const url = req.query.target;
+
+  const browser = await puppeteer.launch({
+    headless: true
+  });
+
+  const webPage = await browser.newPage();
+  
+  
+  // networkidle0, Puppeteer waits until there are 
+  // no new network connections within the last 500 ms
+  // it indicates the site has finished loading.
+  await webPage.goto(url, {
+    waitUntil: "networkidle0"
+  });
+  
+  const pdf = await webPage.pdf({
+    printBackground: true,
+    path: "webpage.pdf",
+    format: "Letter",
+    margin: {
+      top: "20px",
+      bottom: "40px",
+      left: "20px",
+      right: "20px"
+    }
+  })
+  
+  await browser.close();
+
+  res.contentType("application/pdf");
+  res.send(pdf);
+
+})
+
 app.listen(3000, () => {
   console.log("Server started.");
 });
